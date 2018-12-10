866e28f381dc91909e9956a21b42c36a8bebfe83
vipnc12 <lybang0998@gmail.com>
2018-12-10 10:51:49 +0700

    add

--- 1.js
+++ 1.js
@@ -1,31 +1,31 @@
-$(function(){
-	$(window).resize(function(){
-		var docao = $(window).height();
-		$('.anhnen').css({'height':docao})
+﻿ $(function(){
+ 	khoimonan = $('.nhieumon').isotope({
+	  itemSelector: '.motmon',
+	  layoutMode: 'masonry'
 	});
 
-	
-	$('.n1').click(function(){
-		$('html, body').animate({scrollTop:$('.about').offset().top},2000,"easeInOutExpo")
-	})
-	$('.n2').click(function(){
-			$('html, body').animate({scrollTop:$('.taixuong').offset().top},2000,"easeInOutExpo")
-		})
-	$('.n3').click(function(){
-			$('html, body').animate({scrollTop:$('.contact').offset().top},2000,"easeInOutExpo")
-		})
-	$('.nutxuong').click(function(){
-			$('html, body').animate({scrollTop:$('.about').offset().top},2000,"easeInOutExpo")
-		})
-		
-	
-	$(window).scroll(function() {
-		vitrihientai = $('html, body').scrollTop();
+	khoimonan.imagesLoaded().progress( function() {
+	  khoimonan.isotope('layout');
+	});
 
-	if (vitrihientai > 50){
-		$('.navbar-fixed-top').addClass('tienhoa');
-	}
-	else $('.navbar-fixed-top').removeClass('tienhoa');
-})
+	$('.tieudect a').click(function() {
+		dulieu = $(this).data('monan'); 
+	//	console.log(dulieu);
+		khoimonan.isotope({ filter:  dulieu })
+		 return false ; 
+	});
 
-})
+	// xu ly phan responsive bang jquery . 
+	// tinh ra so div ten .motmon
+
+	//soluongmon = $('.motmon').length ;
+	//docao = soluongmon* 120; 
+	//console.log(docao );
+
+	//$('.noidungct').css({'height':docao})
+
+	// xem div .motmon co ton tai hay khong 
+	 
+	 new WOW().init();
+})  
+ 
